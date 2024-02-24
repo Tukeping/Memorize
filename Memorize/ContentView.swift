37fcaef681832d555db6f3a7b9d8f8e930d4ed39
tukeping <tukeping@gmail.com>
2024-02-24 12:06:31 +0800

    Array filled the card

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["👻","🎃","🚀","🥝"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello CS139p")
+        HStack {
+            ForEach(0..<4, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
         }
+        .foregroundColor(.orange)
         .padding()
     }
 }
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack() {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+
 
 #Preview {
     ContentView()
